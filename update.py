1fb60b8a3ce55a3627be3bc023ae0db86e378943
TEMohanish <93865676+TEMohanish@users.noreply.github.com>
2022-10-24 08:44:34 +0530

    Mohanish Commit [V1.6]

--- update.py
+++ update.py
@@ -22,7 +22,7 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
                     handlers=[logging.FileHandler('log.txt'), logging.StreamHandler()],
                     level=logging.INFO)
 
-UPSTREAM_REPO = 'https://github.com/PokeTideBots/WZI-Video-Encoder-Bot'
+UPSTREAM_REPO = 'https://github.com/TEMohanish/TEI-Video-Encode-Bot'
 UPSTREAM_BRANCH = 'beta'
 
 if UPSTREAM_REPO is not None:
